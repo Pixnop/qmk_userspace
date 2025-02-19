9b949e9d458045a022f65c60392f88621de957f4
fievet <fievet.leon@free.fr>
2025-02-19 15:14:29 +0100

    fix virgoule

--- keyboards/bastardkb/charybdis/4x6/keymaps/bepo/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/bepo/keymap.c
@@ -68,7 +68,7 @@
         KC_LCTL,  PT_ECIR, BP_AGRV,    BP_Y,    BP_X,  BP_DOT,       BP_K, BP_QUOT,    BP_Q,    BP_G,    BP_H,    PT_F,
    // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                     KC_LGUI,   KC_SPC,   LOWER,      RAISE,  KC_ENT,
-                                           KC_LSFT, KC_BSPC,         KC_DEL  // KC_RALT pour AltGr
+                                           KC_LSFT, KC_BSPC,         KC_DEL
    //                            ╰───────────────────────────╯ ╰──────────────────╯
    ),
 
@@ -78,20 +78,20 @@
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
          RGB_MOD, BP_CCED,    BP_W, XXXXXXX, XXXXXXX, XXXXXXX,    BP_LBRC,   KC_P7,   KC_P8,   KC_P9, BP_RBRC, XXXXXXX,
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-         RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    BP_PLUS,   KC_P4,   KC_P5,   KC_P6, BP_MINS, BP_EQL,
+         RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    BP_PLUS,   KC_P4,   KC_P5,   KC_P6, BP_MINS,  BP_EQL,
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    BP_ASTR,   KC_P1,   KC_P2,   KC_P3, BP_SLSH, BP_DOT,
    // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                     XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______,
                                             XXXXXXX, XXXXXXX,      KC_P0
    //                            ╰───────────────────────────╯ ╰──────────────────╯
-   )
+   ),
 
    [LAYER_RAISE] = LAYOUT(
    // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
           KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-         KC_MNXT, XXXXXXX, XXXXXXX,  KC_UP, XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
+         KC_MNXT, XXXXXXX, XXXXXXX,  KC_UP, XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
          KC_MPLY, XXXXXXX,KC_LEFT , KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
