e813f1f6929b43abad91767eac555bfbe0008993
Fievetl <fievet.leon@free.fr>
2026-01-31 17:04:49 +0100

    fix(bepo): Use update_tri_layer_state instead of TRI_LAYER feature

--- keyboards/bastardkb/charybdis/4x6/keymaps/bepo/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/bepo/config.h
@@ -26,7 +26,3 @@
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 
-// Tri-layer configuration (Lower + Raise = Adjust)
-#define TRI_LAYER_LOWER_LAYER 1
-#define TRI_LAYER_UPPER_LAYER 2
-#define TRI_LAYER_ADJUST_LAYER 4
